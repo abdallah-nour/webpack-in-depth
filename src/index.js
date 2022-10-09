051a2ff97897dff8b0ba34c98a6102a17fe0a91d
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2022-10-09 14:32:14 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import { join } from "lodash/array";
 import "./style.css";
 import Icon from "./icon.png"
+import printMe from './print.js';
 
-console.log("add assets loaders");
+console.log("output manage");
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
   element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  // button
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  // div style
   element.classList.add('hello');
 
+  // image
   const myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
